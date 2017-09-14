3f8c9b517dc76e15acd5add9abe5eb1e896ec06e
mike_neck <jkrt3333@gmail.com>
2017-09-15 02:11:59 +0900

    やっと接続した…

--- migration-from8/src/test/java/com/example/SampleRunner.java
+++ migration-from8/src/test/java/com/example/SampleRunner.java
@@ -15,5 +15,51 @@
  */
 package com.example;
 
+import com.example.entity.CompanyEntity;
+import com.example.repo.CompanyQuery;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.persist.PersistService;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.flywaydb.core.Flyway;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class SampleRunner {
+
+    private Injector injector;
+
+    @After
+    public void tearDown() {
+        injector.getInstance(PersistService.class).stop();
+    }
+
+    @Before
+    public void setup() throws Exception {
+        injector = Guice.createInjector(new Module());
+        injector.getInstance(PersistService.class).start();
+
+        final Flyway flyway = new Flyway();
+        final MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://localhost:3306/migration_from8?useUnicode=true&characterEncoding=utf8");
+        dataSource.setUser("from8");
+        dataSource.setPassword("from8");
+
+        flyway.setDataSource(dataSource);
+        flyway.migrate();
+    }
+
+    @Test
+    public void runCompanyQuery() {
+        final CompanyQuery query = injector.getInstance(CompanyQuery.class);
+        final Optional<CompanyEntity> company = query.findById(1L);
+        assertThat(company.isPresent(), is(true));
+        company.ifPresent(c -> assertThat(c.getName(), is("サンプル工業")));
+    }
 }
